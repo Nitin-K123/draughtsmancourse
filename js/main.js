b91e5e148f7786bd0fab336be853cc3741b55d2e
Nitin-K123 <nitin14.kohad@gmail.com>
2025-01-22 20:27:26 +0530

    new setup IV

---
+++ js/main.js
@@ -0,0 +1,200 @@
+$(function () {
+
+    "use strict";
+
+    //======topbar js======
+    $(".close_topbar").on("click", function () {
+        $(".topbar").addClass("hide_topbar");
+    });
+
+
+   
+
+    //======menu fix js======
+    var navoff = $('.main_menu').offset().top;
+    $(window).scroll(function () {
+        var scrolling = $(this).scrollTop();
+
+        if (scrolling > navoff) {
+            $('.main_menu').addClass('menu_fix');
+        } else {
+            $('.main_menu').removeClass('menu_fix');
+        }
+    });
+
+
+    //=======CATEGORY SLIDER======
+    $('.category_slider').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 4000,
+        dots: false,
+        arrows: true,
+        nextArrow: '<i class="far fa-angle-right nextArrow"></i>',
+        prevArrow: '<i class="far fa-angle-left prevArrow"></i>',
+        responsive: [
+            {
+                breakpoint: 1400,
+                settings: {
+                    slidesToShow: 4,
+                }
+            },
+            {
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 3,
+                }
+            },
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 2,
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    arrows: false,
+                    slidesToShow: 2,
+                }
+            },
+            {
+                breakpoint: 576,
+                settings: {
+                    arrows: false,
+                    slidesToShow: 1,
+                }
+            }
+        ]
+    });
+    
+    //======= VENOBOX.JS.=========
+    $('.venobox').venobox();
+
+
+    //=======TEAM SLIDER======
+    $('.team_slider').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 4000,
+        dots: true,
+        arrows: false,
+        responsive: [
+            {
+                breakpoint: 1400,
+                settings: {
+                    slidesToShow: 3,
+                }
+            },
+            {
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 3,
+                }
+            },
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 2,
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2,
+                }
+            },
+            {
+                breakpoint: 576,
+                settings: {
+                    arrows: false,
+                    slidesToShow: 1,
+                }
+            }
+        ]
+    });
+
+   
+
+    //=======BLOG SLIDER======
+    $('.blog_slider').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 4000,
+        dots: true,
+        arrows: false,
+        responsive: [
+            {
+                breakpoint: 1400,
+                settings: {
+                    slidesToShow: 4,
+                }
+            },
+            {
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 4,
+                }
+            },
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 3,
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2,
+                }
+            },
+            {
+                breakpoint: 576,
+                settings: {
+                    arrows: false,
+                    slidesToShow: 1,
+                }
+            }
+        ]
+    });
+
+
+    //*=======SCROLL BUTTON=======
+    $('.scroll_btn').on('click', function () {
+        $('html, body').animate({
+            scrollTop: 0,
+        }, 300);
+    });
+
+    $(window).on('scroll', function () {
+        var scrolling = $(this).scrollTop();
+
+        if (scrolling > 500) {
+            $('.scroll_btn').fadeIn();
+        } else {
+            $('.scroll_btn').fadeOut();
+        }
+    });
+
+
+  
+
+    //*========STICKY SIDEBAR=======
+    $("#sticky_sidebar").stickit({
+        top: 90,
+    })
+
+
+    //======wow js=======
+    new WOW().init();
+
+
+    //======MOBILE MENU BUTTON=======
+    $(".navbar-toggler").on("click", function () {
+        $(".navbar-toggler").toggleClass("show");
+    });
+    $("#year").text(new Date().getFullYear()); 
+});
